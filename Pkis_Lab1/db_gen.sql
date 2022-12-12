7a2c6c66e3ed49e590787d65ad833e600dbed1d5
KreshtanovychNikita <90615653+KreshtanovychNikita@users.noreply.github.com>
2022-12-12 12:48:13 +0200

    #lab1

--- Pkis_Lab1/db_gen.sql
+++ Pkis_Lab1/db_gen.sql
@@ -3,49 +3,29 @@
   Firstname varchar(30) NOT NULL,
   MIddlename varchar(30),
   Lastname varchar(30) NOT NULL,
-  Adress varchar(50),
-  Birthday date NOT NULL,
-  PhoneNumber varchar(20) NOT NULL,
   PRIMARY KEY (Id)
 )
 GO
-CREATE TABLE [LibraryUser] (
-  Id integer NOT NULL IdENTITY (1, 1),
-  PersonId integer NOT NULL,
-  PRIMARY KEY (Id),
-  FOREIGN KEY (PersonId) REFERENCES Person(Id),
-)
-GO
-CREATE TABLE [Author] (
+CREATE TABLE [Company] (
   Id integer NOT NULL IDENTITY (1, 1),
-  Firstname varchar(15) NOT NULL,
-  Lastname varchar(15) NOT NULL,
+  CompanyName varchar(15) NOT NULL,
+  PhoneCompanyNumber varchar(15) NOT NULL,
+  AddresCompany varchar(30) NOT NULL,
   PRIMARY KEY (Id)
 )
 GO
-CREATE TABLE [Book] (
+CREATE TABLE [Goods] (
   Id integer NOT NULL IDENTITY (1, 1),
-  BookName varchar(20) NOT NULL,
-  AuthorId integer NOT NULL,
+  GoodsName varchar(20) NOT NULL,
+  CompanyId integer NOT NULL,
+  CalculateV varchar(20) NOT NULL,
+  QuantityOfGoods integer (1000) NOT NULL,
+  SaleTime datetime NOT NULL,
+  Discount integer (20),
   Price float NOT NULL,
+  Description varchar(250),
   PRIMARY KEY (Id),
-  FOREIGN KEY (AuthorId) REFERENCES Author(Id)
+  FOREIGN KEY (CompanyId) REFERENCES Company(Id)
 )
-GO
-CREATE TABLE [BookOutOfStorage] (
-  BookId integer NOT NULL,
-  LibraryUserId integer NOT NULL,
-  TakeAwayTime datetime NOT NULL,
-  GiveAwayTime datetime NOT NULL,
-  IsReturned bit NOT NULL DEFAULT 0,
-  FOREIGN KEY (BookId) REFERENCES Book(Id),
-  FOREIGN KEY (LibraryUserId) REFERENCES LibraryUser(Id)
-)
-GO
-CREATE TABLE [Storage] (
-  BookId integer NOT NULL,
-  AbsoluteAmount integer NOT NULL,
-  CurrentAmount integer NOT NULL,
-  FOREIGN KEY (BookId) REFERENCES Book(Id),
-)
+
 
