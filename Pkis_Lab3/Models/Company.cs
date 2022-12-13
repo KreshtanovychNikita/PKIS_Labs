1d831fedcfd413d819a25785b5d8159d74c85ce1
KreshtanovychNikita <90615653+KreshtanovychNikita@users.noreply.github.com>
2022-12-13 16:45:51 +0200

    #some fix

--- Pkis_Lab3/Models/Company.cs
+++ Pkis_Lab3/Models/Company.cs
@@ -2,9 +2,8 @@ using MongoDB.Bson;
 
 namespace Lab3.Models;
 {
-    internal class Company
+    internal class Company: ModelBase
     {
-        [Key]
         public int Id { get; set; }
         public string CompanyName { get; set; } = null!; 
         public string PhoneCompanyNumber { get; set; }
