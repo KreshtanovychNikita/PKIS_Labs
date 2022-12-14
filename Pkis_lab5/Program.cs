5bf1a68ea7ae9be26a4b55b73cef902c3fbdeb0e
KreshtanovychNikita <90615653+KreshtanovychNikita@users.noreply.github.com>
2022-12-14 17:11:52 +0200

    #some fix

---
+++ Pkis_lab5/Program.cs
@@ -0,0 +1,32 @@
+using Lab5.Data;
+using Lab5.Models;
+using Lab5.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<GoodsCompanyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IRepository<Person>, PersonRepository>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
